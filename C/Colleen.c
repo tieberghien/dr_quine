6d1d3e94d7d101f87734ca70df8bf252e5b7a6dc
admin <admin@iMac-de-admin-8.local>
2018-05-31 20:31:18 +0200

    Colleen done

---
+++ C/Colleen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+   Mon premier commentaire
+*/
+void func(){;}int main(){
+/*
+   Mon second commentaire
+*/char *c="#include <stdio.h>%c/*%c   Mon premier commentaire%c*/%cvoid func(){;}int main(){%c/*%c   Mon second commentaire%c*/char *c=%c%s%c;func();printf(c,10,10,10,10,10,10,10,34,c,34,10);}%c";func();printf(c,10,10,10,10,10,10,10,34,c,34,10);}
