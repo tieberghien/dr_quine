4ade56ec11af5432b5a603c361bc59d2cf425a52
admin <admin@iMac-de-admin-8.local>
2018-12-12 09:46:03 +0100

    halfway there

---
+++ ASM/Grace.s
@@ -0,0 +1,6 @@
+%define a "%define "
+%define b "db "
+%define c "%deftok "
+%define d "a, 97, 32, 34, a, 34, 10, a, 98, 32, 34, b, 34, 10, a, 99, 32, 34, c, 34, 10, a, 100, 32, 34, d, 34, 10, c, 101, 32, 100, 10, b, 101, 10"
+%deftok e d
+db e
