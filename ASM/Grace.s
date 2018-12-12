39ecf3997d38798843d6e1dc6756823d05700ac0
Estelle TIEBERGHIEN <etieberg@e1r8p18.42.fr>
2018-12-12 14:15:55 +0100

    Grace

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,6 +1,30 @@
-%define a "%define "
-%define b "db "
-%define c "%deftok "
-%define d "a, 97, 32, 34, a, 34, 10, a, 98, 32, 34, b, 34, 10, a, 99, 32, 34, c, 34, 10, a, 100, 32, 34, d, 34, 10, c, 101, 32, 100, 10, b, 101, 10"
-%deftok e d
-db e
+section .text
+	global _start          ;must be declared for linker (ld)
+
+_start:                ;tell linker entry point
+	mov rdi, filename
+	mov rsi, 0102o     ;O_CREAT, man open
+	mov rdx, 0666o     ;umode_t
+	mov rax, 2
+	syscall
+
+	mov [fd], rax
+	mov rdx, len       ;message length
+	mov rsi, msg       ;message to write
+	mov rdi, [fd]      ;file descriptor
+	mov rax, 1         ;system call number (sys_write)
+	syscall            ;call kernel
+
+	mov rdi, [fd]
+	mov rax, 3         ;sys_close
+	syscall
+
+	mov rax, 60        ;system call number (sys_exit)
+	syscall            ;call kernel
+
+section .data
+	msg db 'Hello, world', 0xa
+	len equ $ - msg
+	filename db 'test.txt', 0
+	lenfilename equ $ - filename
+	fd dq 0
