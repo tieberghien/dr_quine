24a95ccd66df40920ea7522f9fe1cd7a064100fc
admin <admin@iMac-de-admin-8.local>
2019-01-05 21:37:29 +0100

    slt

--- bonus/Grace.py
+++ bonus/Grace.py
@@ -1,4 +1,8 @@
 kid = 'Grace_kid.py'
-x = "kid = 'Grace_kid.py'{:s}x = {:s}f = open(kid, 'w');f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10))); f.close{:s}    #Superbe{:s}"
-f = open(kid, 'w');f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10))); f.close
+x = "kid = 'Grace_kid.py'{:s}x = {:s}try:{:s}    f = open(kid, 'w');{:s}except IOError:{:s}    print 'eh'{:s}f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10), chr(10), chr(10), chr(10), chr(10))); f.close{:s}    #Superbe{:s}"
+try:
+    f = open(kid, 'w');
+except IOError:
+    print 'eh'
+f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10), chr(10), chr(10), chr(10), chr(10))); f.close
     #Superbe
