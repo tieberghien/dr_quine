8d10ffee5a68162c61db8b2801ec29601a34a4a2
admin <admin@iMac-de-admin-8.local>
2019-01-05 21:14:24 +0100

    python grace done

---
+++ bonus/Grace.py
@@ -0,0 +1,4 @@
+kid = 'Grace_kid.py'
+x = "kid = 'Grace_kid.py'{:s}x = {:s}f = open(kid, 'w');f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10))); f.close{:s}    #Superbe{:s}"
+f = open(kid, 'w');f.write(x.format(chr(10), chr(34)+x+chr(34)+chr(10), chr(10), chr(10))); f.close
+    #Superbe
